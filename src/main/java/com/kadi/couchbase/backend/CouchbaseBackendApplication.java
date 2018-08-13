d158b045cb302ac24c1a51d5cb9d062895fa6e5c
Aymen Mehri <aymen.mehri@soat.fr>
2018-08-13 18:27:30 +0200

    first commit

---
+++ src/main/java/com/kadi/couchbase/backend/CouchbaseBackendApplication.java
@@ -0,0 +1,12 @@
+package com.kadi.couchbase.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouchbaseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CouchbaseBackendApplication.class, args);
+	}
+}
